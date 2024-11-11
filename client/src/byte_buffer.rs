74d4ceb5e0c2fecfedb64780a3a1ebaa563b9afe
张道强 <zdq@meitu.com>
2024-11-11 09:56:55 +0800

    添加静态库配置

--- client/src/byte_buffer.rs
+++ client/src/byte_buffer.rs
@@ -9,32 +9,3 @@ impl ByteBuffer {
         }
     }
 }
-
-#[no_mangle]
-pub extern "C" fn free_byte_buffer(handle: *mut ByteBuffer) {
-    if handle.is_null() {
-        return;
-    }
-
-    unsafe {
-        drop(Box::from_raw(handle));
-    }
-}
-
-#[no_mangle]
-pub extern "C" fn bytes_len(handle: *mut ByteBuffer) -> u64 {
-    if handle.is_null() {
-        return 0;
-    }
-
-    unsafe { (*handle).inner.len() as u64 }
-}
-
-#[no_mangle]
-pub extern "C" fn bytes_content(handle: *mut ByteBuffer) -> *const u8 {
-    if handle.is_null() {
-        return std::ptr::null();
-    }
-
-    unsafe { (*handle).inner.as_ptr() }
-}

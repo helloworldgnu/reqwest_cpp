3ed1f0d5b3054dbe930ee2d4e711fc9b6358f658
lsl <lsl3@meitu.com>
2024-11-13 15:06:26 +0800

    fix: update lipo sh

--- client/lipo.sh
+++ client/lipo.sh
@@ -1,5 +1,5 @@
-lipo -create target/aarch64-apple-darwin/debug/libcrab_http.dylib target/x86_64-apple-darwin/debug/libcrab_http.dylib -output libclient_debug.dylib
-install_name_tool -id @rpath/libcrab_http.dylib libclient_debug.dylib
+lipo -create target/aarch64-apple-darwin/debug/libcrab_http.dylib target/x86_64-apple-darwin/debug/libcrab_http.dylib -output libcrab_http_debug.dylib
+install_name_tool -id @rpath/libcrab_http.dylib libcrab_http_debug.dylib
 
-lipo -create target/aarch64-apple-darwin/release/libcrab_http.dylib target/x86_64-apple-darwin/release/libcrab_http.dylib -output libclient_release.dylib
-install_name_tool -id @rpath/libcrab_http.dylib libclient_release.dylib
+lipo -create target/aarch64-apple-darwin/release/libcrab_http.dylib target/x86_64-apple-darwin/release/libcrab_http.dylib -output libcrab_http_release.dylib
+install_name_tool -id @rpath/libcrab_http.dylib libcrab_http_release.dylib
